115c2ee722c7a59a96a4377a6f5401ebc3668ee1
masato111 <sato88ten@gmail.com>
2026-04-05 08:53:45 +0900

    [modify] globaltsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import { Headding1 } from "@/module/Headding/Headding1";
+import { HeaddingUnderLine2 } from "@/module/Headding/Headding2";
 
 export default function Home() {
   return (
     <section>
       <Headding1>Headding1</Headding1>
+      <HeaddingUnderLine2 param={{ class: "red" }}>
+        Headding1
+      </HeaddingUnderLine2>
+      <p className="txt">
+        ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+      </p>
+      <p className="txt">
+        ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+      </p>
     </section>
   );
 }
